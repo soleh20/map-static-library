0eb4581648469e0b2dd4f0c813348ee97a9f7bd8
soleh setiawan <54166576+soleh20@users.noreply.github.com>
2021-11-29 16:16:58 +0700

    main

---
+++ src/strategies/direction/nativeStrategy.js
@@ -0,0 +1,38 @@
+function nativeStrategy(data) {
+  const { origin, destination, travelMode } = data;
+
+  let originLocation;
+  let destinationLocation;
+
+  if (typeof origin === 'object' && origin.lat && origin.lng) {
+    originLocation = new google.maps.LatLng(origin);
+  } else {
+    originLocation = origin;
+  }
+
+  if (typeof destination === 'object' && destination.lat && destination.lng) {
+    destinationLocation = new google.maps.LatLng(destination);
+  } else {
+    destinationLocation = destination;
+  }
+
+  const DirectionsService = new google.maps.DirectionsService();
+  return new Promise((resolve, reject) => {
+    DirectionsService.route(
+      {
+        origin: originLocation,
+        destination: destinationLocation,
+        travelMode: travelMode.toUpperCase(),
+      },
+      (result, status) => {
+        if (status === google.maps.DirectionsStatus.OK) {
+          resolve(result.routes[0].overview_polyline);
+        }
+
+        reject(status);
+      }
+    );
+  });
+}
+
+export default nativeStrategy;
